f49ef805afc2d83caebb5d97da6607a0a453c5a9
ronnie-z <80464340+ronnie-z@users.noreply.github.com>
2022-02-21 20:26:17 +0800

    modifying train.py

--- train.py
+++ train.py
@@ -33,9 +33,9 @@ train_trainsform = transforms.Compose([
         # transforms.RandomCrop(size=64, pad_if_needed=True),
     ])
 
-train_dataset = DataSet(r'D:\code\imagefusion-rfn-nest\images\40_pairs_tno_vot_split\vis', train_trainsform)
+train_dataset = DataSet('/data/data_zkl/40_pairs_tno_vot_split/vis', train_trainsform)
 
-train_loader = data.DataLoader(train_dataset, batch_size = 4, shuffle=True, num_workers = 2, drop_last = True)
+train_loader = data.DataLoader(train_dataset, batch_size = 64, shuffle=True, num_workers = 2, drop_last = True)
 
 def inf_train_gen():
     while True:
@@ -48,9 +48,9 @@ if __name__ == '__main__':
     netD_ir = Discriminator().cuda()
 
     # 优化器
-    optimizerG = optim.Adam(netG.parameters(), lr = 1e-3, betas = (0.5, 0.9))
-    optimizerD_vis = optim.Adam(netD_vis.parameters(), lr = 1e-3, betas = (0.5, 0.9))
-    optimizerD_ir = optim.Adam(netD_ir.parameters(), lr = 1e-3, betas = (0.5, 0.9))
+    optimizerG = optim.Adam(netG.parameters(), lr = 1e-4, betas = (0.5, 0.9))
+    optimizerD_vis = optim.Adam(netD_vis.parameters(), lr = 1e-4, betas = (0.5, 0.9))
+    optimizerD_ir = optim.Adam(netD_ir.parameters(), lr = 1e-4, betas = (0.5, 0.9))
 
     # loss
     pix_loss_fr = nn.MSELoss()
@@ -71,11 +71,11 @@ if __name__ == '__main__':
             p.requires_grad = True
 
         for i in range(CRITIC_ITERS):
-            _data = data.next()
+            _data = next(data)
             vis_img = _data[0].cuda()
             ir_img = _data[1].cuda()
             vis_list = netG.encoder(vis_img)
             ir_list = netG.encoder(ir_img)
             fusion_img = netG.decoder(vis_list, ir_list)
             print(fusion_img.shape)
-            break
+            # break
